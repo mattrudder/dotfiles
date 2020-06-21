5b8f7397ee3378d0f425fff59079a6201a4d148d
Matt Rudder <rudderm@twitch.tv>
2020-06-21 12:40:25 -0500

    Fixing install script on Ubuntu

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function link {
   local src=$1
   local dest=$2
 
-  ln -sFhi "$src" "$dest"
+  ln -sFi "$src" "$dest"
 }
 
 # vim configs
@@ -29,18 +29,18 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # .configs
-ln -sFhi $SCRIPT_PATH/fish/ ~/.config/
-ln -sFhi $SCRIPT_PATH/nvim/ ~/.config/
+link $SCRIPT_PATH/fish/ ~/.config/
+link $SCRIPT_PATH/nvim/ ~/.config/
 
 # zsh configs
-ln -sFhi $SCRIPT_PATH/zsh/.zshenv ~/.zshenv
-ln -sFhi $SCRIPT_PATH/zsh ~/.zsh
+link $SCRIPT_PATH/zsh/.zshenv ~/.zshenv
+link $SCRIPT_PATH/zsh ~/.zsh
 
 
 # Git configs
-ln -sFhi $SCRIPT_PATH/gitconfig ~/.gitconfig
-ln -sFhi $SCRIPT_PATH/githelpers ~/.githelpers
-ln -sFhi $SCRIPT_PATH/gitignore ~/.gitignore
+link $SCRIPT_PATH/gitconfig ~/.gitconfig
+link $SCRIPT_PATH/githelpers ~/.githelpers
+link $SCRIPT_PATH/gitignore ~/.gitignore
 
 which rustup >/dev/null || curl --proto '=https' --tlsv1.2 -sS https://sh.rustup.rs | sh
 which starship >/dev/null || cargo install starship --force
