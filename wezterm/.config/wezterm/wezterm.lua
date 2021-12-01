aeba893b7217f91a3ef71796182c0c3755c0f3d6
Matt Rudder <matt@mattrudder.com>
2021-11-30 19:09:55 -0600

    wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,42 @@
 local wezterm = require 'wezterm';
 
+-- A helper function for my fallback fonts
+local function font_with_fallback(name, params)
+  local names = {name, "Noto Color Emoji"}
+  return wezterm.font_with_fallback(names, params)
+end
 
 return {
-    color_scheme = "Wombat",
-    font = wezterm.font_with_fallback({
-        "MonoLisa",
-    }),
+  enable_tab_bar = true,
+  window_background_opacity = 0.95,
+  color_scheme = "Wombat",
+  harfbuzz_features = {"liga=0"},
+  font = font_with_fallback("MonoLisa NF", {weight="Medium"}),
+  font_rules= {
+    -- Select a fancy italic font for italic text
+    {
+      italic = true,
+      font = font_with_fallback("MonoLisa NF", {weight="Medium", italic=true}),
+    },
+
+    -- Similarly, a fancy bold+italic font
+    {
+      italic = true,
+      intensity = "Bold",
+      font = font_with_fallback("MonoLisa NF", {weight="Black", italic=true}),
+    },
+
+    -- Make regular bold text a different color to make it stand out even more
+    {
+      intensity = "Bold",
+      font = font_with_fallback("MonoLisa NF", {weight="Black", foreground="tomato"}),
+    },
+
+    -- For half-intensity text, use a lighter weight font
+    {
+      intensity = "Half",
+      font = font_with_fallback("MonoLisa NF", {weight="Light"}),
+    },
+  },
 }
+
