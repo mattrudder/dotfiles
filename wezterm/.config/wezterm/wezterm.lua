59e2a40032cff17a47987b4c4cef21192264e00c
Matt Rudder <matt@mattrudder.com>
2021-11-30 19:21:48 -0600

    wezterm: configs now use symbol-only version of Nerd Fonts as fallback

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 -- A helper function for my fallback fonts
 local function font_with_fallback(name, params)
-  local names = {name, "Noto Color Emoji"}
+  local names = {name, "Noto Color Emoji", "Symbols Nerd Font"}
   return wezterm.font_with_fallback(names, params)
 end
 
@@ -11,31 +11,31 @@ return {
   window_background_opacity = 0.95,
   color_scheme = "Wombat",
   harfbuzz_features = {"liga=0"},
-  font = font_with_fallback("MonoLisa NF", {weight="Medium"}),
+  font = font_with_fallback("MonoLisa", {weight="Medium"}),
   font_rules= {
     -- Select a fancy italic font for italic text
     {
       italic = true,
-      font = font_with_fallback("MonoLisa NF", {weight="Medium", italic=true}),
+      font = font_with_fallback("MonoLisa", {weight="Medium", italic=true}),
     },
 
     -- Similarly, a fancy bold+italic font
     {
       italic = true,
       intensity = "Bold",
-      font = font_with_fallback("MonoLisa NF", {weight="Black", italic=true}),
+      font = font_with_fallback("MonoLisa", {weight="Black", italic=true}),
     },
 
     -- Make regular bold text a different color to make it stand out even more
     {
       intensity = "Bold",
-      font = font_with_fallback("MonoLisa NF", {weight="Black", foreground="tomato"}),
+      font = font_with_fallback("MonoLisa", {weight="Black", foreground="tomato"}),
     },
 
     -- For half-intensity text, use a lighter weight font
     {
       intensity = "Half",
-      font = font_with_fallback("MonoLisa NF", {weight="Light"}),
+      font = font_with_fallback("MonoLisa", {weight="Light"}),
     },
   },
 }
