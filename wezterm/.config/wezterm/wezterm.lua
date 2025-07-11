a90137c1326cf6978acf33db96025b7c35b4099d
Matt Rudder <matt@mattrudder.com>
2025-07-10 23:25:08 -0500

    update wezterm with new configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,66 +1,75 @@
 local wezterm = require 'wezterm'
-local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
+local config = {
+  color_scheme = 'Wombat',
+  front_end = 'WebGpu',
+  webgpu_power_preference = 'HighPerformance',
+  window_background_opacity = 0.8,
+  win32_system_backdrop = 'Acrylic',
+  -- freetype_load_target = 'HorizontalLcd',
+  window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+  max_fps = 240,
+  window_padding = {
+    top = 8, left = 8, bottom = 8, right = 8
+  },
+  command_palette_font_size = 12,
+  command_palette_bg_color = '#394b70',
+  command_palette_fg_color = '#828bb8',
+  ui_key_cap_rendering = 'WindowsSymbols'
+}
 local launch_menu = {}
-
-config.window_background_opacity = 0.95
-config.color_scheme = "Wombat"
 
 wezterm.log_info('triple = ' .. wezterm.target_triple)
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  local pwsh = wezterm.home_dir ..
-      '\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe'
-  wezterm.log_info('pwsh = ' .. pwsh)
+  local pwsh = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
   table.insert(launch_menu, {
     label = 'PowerShell',
-    args = { pwsh }
+    args = { pwsh, '-NoLogo' }
   })
 
-  config.default_prog = { pwsh }
+  config.default_prog = { pwsh, '-NoLogo' }
 end
 
 config.launch_menu = launch_menu
 
 -- A helper function for my fallback fonts
 local function font_with_fallback(name, params)
-  local names = { name, "Noto Color Emoji", "Symbols Nerd Font" }
+  local names = { name, 'Noto Color Emoji', 'Symbols Nerd Font' }
   return wezterm.font_with_fallback(names, params)
 end
 
-config.harfbuzz_features = { "zero=1" }
+config.harfbuzz_features = { 'zero=1' }
 config.font_size = 12.0
-local font_family = "Berkeley Mono"
--- local font_family = "Ubuntu Mono"
--- config.font = font_with_fallback("Berkeley Mono", { weight = "Medium" })
-config.font = font_with_fallback(font_family, { weight = "Medium" })
+local font_family = 'GeistMono NF'
+-- local font_family = 'Berkeley Mono'
+-- local font_family = 'Ubuntu Mono'
+-- config.font = font_with_fallback('Berkeley Mono', { weight = 'Medium' })
+config.font = font_with_fallback(font_family, { weight = 'Medium' })
+config.command_palette_font = config.font
 config.font_rules = {
   -- Select a fancy italic font for italic text
   {
     italic = true,
-    font = font_with_fallback(font_family, { weight = "Medium", italic = true }),
+    font = font_with_fallback(font_family, { weight = 'Medium', italic = true }),
   },
 
   -- Similarly, a fancy bold+italic font
   {
     italic = true,
-    intensity = "Bold",
-    font = font_with_fallback(font_family, { weight = "Black", italic = true }),
+    intensity = 'Bold',
+    font = font_with_fallback(font_family, { weight = 'Black', italic = true }),
   },
 
   -- Make regular bold text a different color to make it stand out even more
   {
-    intensity = "Bold",
-    font = font_with_fallback(font_family, { weight = "Black", foreground = "tomato" }),
+    intensity = 'Bold',
+    font = font_with_fallback(font_family, { weight = 'Black', foreground = 'tomato' }),
   },
 
   -- For half-intensity text, use a lighter weight font
   {
-    intensity = "Half",
-    font = font_with_fallback(font_family, { weight = "Light" }),
+    intensity = 'Half',
+    font = font_with_fallback(font_family, { weight = 'Light' }),
   },
 }
 
